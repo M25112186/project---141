89f6951b0351c1d5651c66e204155b9a70faf657
m25112186 <99669367+M25112186@users.noreply.github.com>
2023-06-02 20:54:13 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+# NASA Exoplanet URL
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+
+# Webdriver
+browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
+browser.get(START_URL)
+
+time.sleep(10)
+
+planets_data = []
+
+# Define Exoplanet Data Scrapping Method
+def scrape():
+
+    for i in range(0,10):
+        print(f'Scrapping page {i+1} ...' )
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        for th_tag in soup.find_all("th", attrs={"class", "exoplanet"}):
+            tr_tags = th_tag.find_all("tr")
+            temp_list = []
+            for index, tr_tag in enumerate(tr_tags):
+                if index == 0:
+                    temp_list.append(tr_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(tr_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planets_data.append(temp_list)
+        browser.find_element(by=By.XPATH, value='//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+        
+# Calling Method    
+scrape()
+
+# Define Header
+headers = ["v_mag", "proper_name", "bayer_designation", "distance", "spectral_class", "mass", "radius", "luminosity"]
+
+# Define pandas DataFrame   
+planet_df_1 = pd.DataFrame(planets_data, columns=headers)
+
+# Convert to CSV
+planet_df_1.to_csv('scraped_data.csv', index=True, index_label="id")
+
+
+
